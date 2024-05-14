9cf3b94fd5c03ead24db084b08a5eb6869e7ee3e
AnushaDeviR <anusha3599@gmail.com>
2024-05-14 22:13:09 +0100

    project boiler plate

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<main>
+  <h1>Hello!</h1>
+</main>
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<header>
+    <h1>
+        <img src="" alt="poll logo" />
+    </h1>
+</header>
+
+<style>
+    
+</style>
