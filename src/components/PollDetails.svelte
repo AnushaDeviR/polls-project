5fc6054d75daa7e2e5d0def64770a005746a198c
AnushaDeviR <anusha3599@gmail.com>
2024-06-23 00:34:04 +0100

    added reactive percentage to votes

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -3,10 +3,12 @@
   import Card from "../reuseable/Card.svelte";
   export let poll = [];
 
-  const dispatch = createEventDispatcher();
-
   //   reactive values:
-  $: totalValues = poll.votesA + poll.votesB;
+  $: totalVotes = poll.votesA + poll.votesB;
+  $: percentageA = Math.floor((100 / totalVotes) * poll.votesA);
+  $: percentageB = Math.floor((100 / totalVotes) * poll.votesB);
+
+  const dispatch = createEventDispatcher();
 
   const dispatchVotes = (option, id) => {
     dispatch("vote", { option, id });
@@ -16,18 +18,16 @@
 <Card>
   <div class="poll">
     <h3>{poll.question}</h3>
-    <p>Total votes: {totalValues}</p>
+    <p>Total votes: {totalVotes}</p>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => dispatchVotes("a", poll.id)}>
-      <div class="percent percent-a">
-        <span>{poll.answerA} ({poll.votesA})</span>
-      </div>
+      <div class="percent percent-a" style="width: {percentageA}%"></div>
+      <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => dispatchVotes("b", poll.id)}>
-      <div class="percent percent-b">
-        <span>{poll.answerB} ({poll.votesB})</span>
-      </div>
+      <div class="percent percent-b" style="width: {percentageB}%"></div>
+      <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
 </Card>
@@ -55,5 +55,19 @@
   span {
     display: inline-block;
     padding: 10px 20px;
+  }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+  .percent-a {
+    background: rgb(147, 182, 182, 0.5);
+    border-left: 4px solid rgb(147, 182, 182);
+  }
+  .percent-b {
+    background: rgb(239, 212, 103, 0.5);
+    border-left: 4px solid rgb(239, 212, 103);
   }
 </style>
